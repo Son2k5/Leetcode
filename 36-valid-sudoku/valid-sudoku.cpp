baae1d0a0955a019bc44e9a1b7841f55bbb7fac0
Haucter <sonct2k3@gmail.com>
2025-02-12 08:49:08 +0700

    Time: 4 ms (52.81%) | Memory: 23.8 MB (53.52%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<char> >rows(9), cols(9), boxs(9);
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                char num = board[i][j];
-                if(num == '.') continue;
-                int box_index =i/3 *3 + j/3;
-                if(rows[i].count(num) || cols[j].count(num) ||  boxs[box_index].count(num) ) {
-                    return false;
+        for( int row = 0; row < 9 ; row++){
+            unordered_set<char> present;
+            for(int i = 0; i< 9;i++){
+                if(board[row][i]!='.') {
+                    if(present.count(board[row][i])){
+                        return false;
+                    }
+                    present.insert(board[row][i]);
                 }
-                rows[i].insert(num);
-                cols[j].insert(num);
-                boxs[box_index].insert(num);
+                
             }
         }
+        for(int col = 0; col < 9; col++ ){
+            unordered_set<char> cols;
+            for(int i =0; i<9; i++){
+                if(board[i][col] != '.'){
+                    if(cols.count(board[i][col])){
+                        return false;
+                    }
+                    cols.insert(board[i][col]);
+                }
+            }
+        }
+        for(int square = 0; square < 9; square++){
+            unordered_set<char> boxs;
+            for(int i =0 ; i < 3; i++){
+                for(int j = 0; j< 3; j++){
+                    int row= (square / 3) * 3+ i;
+                    int col= (square % 3) * 3+ j ;
+                    if(board[row][col]== '.') continue;
+                    if(boxs.count(board[row][col])) return false;
+                    boxs.insert(board[row][col]);
+
+                }
+            }   
+        }
         return true;
+
     }
 };
